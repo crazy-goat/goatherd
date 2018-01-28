fdc105aebd7804ab780fa728b2a8e62202a4eccd
Piotr Hałas <Piotr.Halas@komputronik.pl>
2018-01-28 17:44:55 +0100

    minor fix for alpine

--- vendor/initboost.sh
+++ vendor/initboost.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 if [ ! -d "boost" ]; then
     if [ ! -f "boost.tar.gz" ]; then
         wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz -O boost.tar.gz
